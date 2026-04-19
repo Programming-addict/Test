f887f427320028c57a26413bd4521fe3924f04ad
Programming-addict <benzathepiratefox@gmail.com>
2026-04-19 14:47:07 +0700

    feat: export Firebase auth instance

--- test/src/lib/firebase.ts
+++ test/src/lib/firebase.ts
@@ -1,25 +1,17 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { getMessaging, isSupported } from "firebase/messaging";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyDksAWQzMxxtCY6i9_d8XcEhR_w2FsbzZk",
-
   authDomain: "todo-list-84212.firebaseapp.com",
-
   projectId: "todo-list-84212",
-
   storageBucket: "todo-list-84212.firebasestorage.app",
-
   messagingSenderId: "272020296942",
-
   appId: "1:272020296942:web:fb8afd131b305c003f8fff",
-
   measurementId: "G-WEYYSG6QLJ"
-
 };
-
 
 function initFirebase() {
   if (!getApps().length) {
@@ -30,6 +22,7 @@ function initFirebase() {
 
 const app = initFirebase();
 export const db = getFirestore(app);
+export const auth = getAuth(app);
 
 // Messaging is optional — only available in browsers that support it
 export async function setupMessaging() {
