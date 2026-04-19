cb37b2f680b64240d40dbaedc9d18b072a7d2dfb
Programming-addict <benzathepiratefox@gmail.com>
2026-04-19 14:50:17 +0700

    feat: wrap layout with AuthProvider

--- test/src/app/layout.tsx
+++ test/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "../features/settings/context/ThemeContext";
+import { AuthProvider } from "../features/auth/context/AuthContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -29,7 +30,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <AuthProvider>{children}</AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
