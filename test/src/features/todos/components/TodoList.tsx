6edef05ffb8baf02826dc0e1ad8fe1d2c7f6d0ce
Programming-addict <benzathepiratefox@gmail.com>
2026-04-19 14:35:24 +0700

    feat: polish TodoList with todo-list container class

--- test/src/features/todos/components/TodoList.tsx
+++ test/src/features/todos/components/TodoList.tsx
@@ -3,11 +3,11 @@ import TodoItem from "./TodoItem";
 
 export default function TodoList({ todos }: { todos: any[] }) {
   if (!todos.length) {
-    return <div className="card muted">No todos yet. Add one above.</div>;
+    return <div className="todo-empty">No todos yet. Add one above.</div>;
   }
 
   return (
-    <div className="card w-full max-w-3xl">
+    <div className="todo-list">
       {todos.map((t) => (
         <TodoItem key={t.id} todo={t} />
       ))}
